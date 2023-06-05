290bf9e98c09152044747b186f876252e72acdd0
Joseph Bassey John <60972495+joeblinx39@users.noreply.github.com>
2023-06-05 09:15:40 +0000

    django template for github codespaces

---
+++ core/website/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        "title": "Django example",
+    }
+    return render(request, "index.html", context)
